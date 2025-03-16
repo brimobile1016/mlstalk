268e35b744c77f94c142a9e3a1fec0b5e2581af2
BANK BRI <152972674+brimobile1016@users.noreply.github.com>
2025-03-16 13:54:14 +0700

    Update ffstalk.js

--- api/ffstalk.js
+++ api/ffstalk.js
@@ -5,16 +5,18 @@ export default async function handler(req, res) {
         return res.status(405).json({ error: "Metode tidak diizinkan" });
     }
 
+    const id = req.query.id;
+    if (!id) {
+        return res.status(400).json({ error: "Parameter ID diperlukan" });
+    }
+
     try {
-        const { id } = req.query;
-        if (!id) return res.status(400).json({ error: "Parameter ID diperlukan" });
-
         const apiUrl = `https://saipulanuar.eu.org/api/api.php/ffstalk?id=${id}&apikey=bear`;
-        const response = await axios.get(apiUrl);
+        const { data } = await axios.get(apiUrl);
 
-        res.status(200).json(response.data);
+        return res.status(200).json(data);
     } catch (error) {
-        console.error("Error:", error.message);
-        res.status(500).json({ error: "Gagal mengambil data dari API eksternal" });
+        console.error("Error saat fetch:", error.message);
+        return res.status(500).json({ error: "Gagal mengambil data dari API eksternal" });
     }
 }
