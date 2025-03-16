287c9f96ab17421d9c92f78eac8e0719c9ce1527
BANK BRI <152972674+brimobile1016@users.noreply.github.com>
2025-03-16 13:52:00 +0700

    Update ffstalk.js

--- api/ffstalk.js
+++ api/ffstalk.js
@@ -7,18 +7,11 @@ export default async function handler(req, res) {
 
     try {
         const { id } = req.query;
+        if (!id) return res.status(400).json({ error: "Parameter ID diperlukan" });
 
-        if (!id) {
-            return res.status(400).json({ error: "Parameter ID diperlukan" });
-        }
-
-        // URL API target
         const apiUrl = `https://saipulanuar.eu.org/api/api.php/ffstalk?id=${id}&apikey=bear`;
-
-        // Fetch data dari API eksternal menggunakan Axios
         const response = await axios.get(apiUrl);
-        
-        // Kirim hasil JSON ke response
+
         res.status(200).json(response.data);
     } catch (error) {
         console.error("Error:", error.message);
